c45d10611195242ee2513543697cc7bf26828133
Al Zaudtke <zaudtke@gmail.com>
2025-07-25 16:42:44 -0400

    update program, add routeextensions

---
+++ DotNet.MinimalApi.Htmx/Infrastructure/RouteExtensions.cs
@@ -0,0 +1,25 @@
+namespace DotNet.MinimalApi.Htmx.Infrastructure;
+
+public interface IRouteDefinition
+{
+    IEndpointRouteBuilder MapRoutes(IEndpointRouteBuilder endpoints);
+}
+
+public static class RouteExtensions
+{
+    public static IEndpointRouteBuilder MapApplicationRoutes(this IEndpointRouteBuilder routeBuilder)
+    {
+        var routeDefinitions = typeof(Program).Assembly
+            .GetTypes()
+            .Where(t => t.IsAssignableTo(typeof(IRouteDefinition))
+                        && t is { IsAbstract: false, IsInterface: true })
+            .Select(Activator.CreateInstance)
+            .Cast<IRouteDefinition>();
+        foreach (var routeDefinition in routeDefinitions)
+        {
+            routeDefinition.MapRoutes(routeBuilder);
+        }
+
+        return routeBuilder;
+    }
+}
