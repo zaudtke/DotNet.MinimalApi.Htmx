13fc609909b9b34265f71556df451e5fdc1b4c96
Al Zaudtke <zaudtke@gmail.com>
2025-08-16 16:42:09 -0400

    feat: implement ViewContact component and update routing for Click To Edit

--- DotNet.MinimalApi.Htmx/Features/Examples/ClickToEdit/Index.razor.cs
+++ DotNet.MinimalApi.Htmx/Features/Examples/ClickToEdit/Index.razor.cs
@@ -1,3 +1,4 @@
+using DotNet.MinimalApi.Htmx.Data;
 using DotNet.MinimalApi.Htmx.Infrastructure;
 using Microsoft.AspNetCore.Http.HttpResults;
 
@@ -7,7 +8,12 @@ public partial class Index : IRouteDefinition
 {
     public IEndpointRouteBuilder MapRoutes(IEndpointRouteBuilder endpoints)
     {
-        endpoints.MapGet("/Examples/ClickToEdit", () => new RazorComponentResult<Index>());
+        endpoints.MapGet("/Examples/ClickToEdit/{id:int}",
+            async (int id, ContactService service) =>
+            {
+                var contact = await service.GetById(id);
+                return new RazorComponentResult<Index>(new { Contact = contact });
+            });
 
         return endpoints;
     }
